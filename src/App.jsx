ef29c14336918ad80592cda051b338fc8352b4d5
dp <codewithdp2073@gmail.com>
2025-06-28 12:02:49 +0530

    first commit with folder step up

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="h-screen w-full bg-zinc-900 text-zinc-50">
+      <h1 className="text-5xl text-center pt-2">home</h1>
+    </div>
+  );
+};
+
+export default App;
