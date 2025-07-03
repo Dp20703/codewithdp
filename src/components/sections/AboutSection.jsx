88324e4409a71d3b748785ad507a60e1e3c9beec
dp <codewithdp2073@gmail.com>
2025-07-03 12:47:50 +0530

    feat: update project structure and enhance animations; integrate AnimatedContent component in AboutSection, ContactSection, and ProjectCard for improved visual effects

--- src/components/sections/AboutSection.jsx
+++ src/components/sections/AboutSection.jsx
@@ -1,5 +1,5 @@
-import React from "react";
 import PixelTransition from "../../blocks/Animations/PixelTransition/PixelTransition";
+import AnimatedContent from "../../blocks/Animations/AnimatedContent/AnimatedContent";
 
 const AboutSection = () => {
   return (
@@ -8,61 +8,87 @@ const AboutSection = () => {
       style={{ padding: "2rem 4rem" }}
     >
       <div className="w-1/2">
-        <div className="w-[35rem]">
-          <PixelTransition
-            firstContent={
-              <img
-                src="https://plus.unsplash.com/premium_photo-1746927715759-03f68bbd8c9a?q=80&w=744&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="contact"
-                className=" rounded-xl h-[40rem] w-full object-cover object-right"
-              />
-            }
-            gridSize={12}
-            pixelColor="#ffffff"
-            animationStepDuration={0.4}
-            className="custom-pixel-card"
-          />
-        </div>
+        <AnimatedContent
+          distance={150}
+          direction="horizontal"
+          reverse={true}
+          duration={1.5}
+          ease="power.out"
+          initialOpacity={0.5}
+          animateOpacity
+          scale={1}
+          threshold={0.3}
+          delay={0.5}
+        >
+          <div className="w-[35rem]">
+            <PixelTransition
+              firstContent={
+                <img
+                  src="https://plus.unsplash.com/premium_photo-1746927715759-03f68bbd8c9a?q=80&w=744&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt="contact"
+                  className=" rounded-xl h-[40rem] w-full object-cover object-right"
+                />
+              }
+              gridSize={12}
+              pixelColor="#ffffff"
+              animationStepDuration={0.4}
+              className="custom-pixel-card"
+            />
+          </div>
+        </AnimatedContent>
       </div>
       <div className="w-1/2">
-        <div
-          style={{
-            padding: "2rem 2rem",
-            border: ".1rem solid gray",
-          }}
-          className=" h-[40rem] w-full rounded-xl"
+        <AnimatedContent
+          distance={150}
+          direction="horizontal"
+          reverse={false}
+          duration={1.8}
+          ease="power.out"
+          initialOpacity={0.5}
+          animateOpacity
+          scale={1}
+          threshold={0.2}
+          delay={0.5}
         >
-          <div className="w-full h-1/5">
-            <h2 className="text-[2rem] text-[#ffffff] font-bold">About</h2>
-            <h3
-              className="text-xl text-[#444444] "
-              style={{ margin: ".5rem 0" }}
-            >
-              Get to Know Me
-            </h3>
+          <div
+            style={{
+              padding: "2rem 2rem",
+              border: ".1rem solid gray",
+            }}
+            className=" h-[40rem] w-full rounded-xl"
+          >
+            <div className="w-full h-1/5">
+              <h2 className="text-[2rem] text-[#ffffff] font-bold">About</h2>
+              <h3
+                className="text-xl text-[#444444] "
+                style={{ margin: ".5rem 0" }}
+              >
+                Get to Know Me
+              </h3>
+            </div>
+            <div className="w-full text-[#bfbfbf] text-[1.1rem]">
+              <p>
+                I'm Darshan Prajapati, a dedicated MERN stack developer with a
+                passion for crafting clean and functional web applications.
+                Having recently completed my BCA, I’ve been constantly improving
+                my skills by building real-world projects like StockTally and
+                Property-Renting. I enjoy turning complex problems into smooth
+                user experiences and love working across both frontend and
+                backend technologies.
+              </p>
+              <br />
+              <br />
+              <p>
+                Beyond just writing code, I'm deeply focused on learning modern
+                tools, UI/UX best practices, and performance optimization.
+                Whether it’s developing a full-stack project or designing
+                layouts with TailwindCSS, I strive to build scalable and
+                maintainable solutions. I’m also preparing for my MCA while
+                actively exploring new opportunities to grow as a developer.
+              </p>
+            </div>
           </div>
-          <div className="w-full text-[#bfbfbf] text-[1.1rem]">
-            <p>
-              I'm Darshan Prajapati, a dedicated MERN stack developer with a
-              passion for crafting clean and functional web applications. Having
-              recently completed my BCA, I’ve been constantly improving my
-              skills by building real-world projects like StockTally and
-              Property-Renting. I enjoy turning complex problems into smooth
-              user experiences and love working across both frontend and backend
-              technologies.
-            </p>
-            <br />
-            <br />
-            <p>
-              Beyond just writing code, I'm deeply focused on learning modern
-              tools, UI/UX best practices, and performance optimization. Whether
-              it’s developing a full-stack project or designing layouts with
-              TailwindCSS, I strive to build scalable and maintainable
-              solutions. I’m also preparing for my MCA while actively exploring
-              new opportunities to grow as a developer.
-            </p>
-          </div>
-        </div>
+        </AnimatedContent>
       </div>
     </div>
   );
--- src/components/sections/ProjectCard.jsx
+++ src/components/sections/ProjectCard.jsx
@@ -1,4 +1,5 @@
 import GlareHover from "../../blocks/Animations/GlareHover/GlareHover";
+import AnimatedContent from "../../blocks/Animations/AnimatedContent/AnimatedContent";
 const ProjectCard = () => {
   const projects = [
     {
@@ -55,71 +56,84 @@ const ProjectCard = () => {
       {projects.map((project, idx) => {
         return (
           <>
-            <div
-              key={idx}
-              className="h-[38rem] w-[28rem] overflow-hidden flex flex-col  gap-8 bg-[#232323]  rounded-xl"
-              style={{ padding: "2rem 2rem", border: ".1rem solid gray" }}
+            <AnimatedContent
+              distance={150}
+              direction="vertical"
+              reverse={false}
+              duration={1.3}
+              ease="power.out"
+              initialOpacity={0.5}
+              animateOpacity
+              scale={1}
+              threshold={0.2}
+              delay={0.3}
             >
               <div
-                className="bg-[#e2e2e2] h-54 w-52 rounded-2xl overflow-hidden"
-                style={{ border: ".1rem solid gray" }}
+                key={idx}
+                className="h-[38rem] w-[28rem] overflow-hidden flex flex-col  gap-8 bg-[#232323]  rounded-xl"
+                style={{ padding: "2rem 2rem", border: ".1rem solid gray" }}
               >
-                <GlareHover
-                  height="13.5rem"
-                  width="13rem"
-                  glareColor="#ffffff"
-                  glareOpacity={0.3}
-                  glareAngle={-30}
-                  glareSize={300}
-                  transitionDuration={800}
-                  playOnce={false}
+                <div
+                  className="bg-[#e2e2e2] h-54 w-52 rounded-2xl overflow-hidden"
+                  style={{ border: ".1rem solid gray" }}
                 >
-                  <img
-                    src={project.imgURL}
-                    alt={project.title}
-                    className="object-cover w-full h-full"
-                  />
-                </GlareHover>
-              </div>
-
-              <div className="h-1/2 w-full flex flex-col gap-5 justify-center">
-                <div className="flex justify-between items-center">
-                  <h2 className="text-[#ffffff] text-2xl">{project.title}</h2>
-                  <div className="flex items-center justify-around w-1/4">
-                    <a
-                      href={project.github}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <img
-                        src="/assets/icons/github-30.png"
-                        className="h-8 w-8
-                    inline bg-white rounded-full "
-                        alt="github"
-                      />{" "}
-                    </a>
-                    <a
-                      href={project.live}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <span
-                        className="bg-[#444444] hover:bg-[#232323] rounded-full text-xl "
-                        style={{
-                          padding: ".2rem .6rem",
-                          border: ".1rem solid white",
-                        }}
-                      >
-                        <i className="ri-arrow-right-up-long-line text-2xl " />
-                      </span>
-                    </a>
-                  </div>
+                  <GlareHover
+                    height="13.5rem"
+                    width="13rem"
+                    glareColor="#ffffff"
+                    glareOpacity={0.3}
+                    glareAngle={-30}
+                    glareSize={300}
+                    transitionDuration={800}
+                    playOnce={false}
+                  >
+                    <img
+                      src={project.imgURL}
+                      alt={project.title}
+                      className="object-cover w-full h-full"
+                    />
+                  </GlareHover>
                 </div>
-                <p className="text-[#bfbfbf] text-[1.1rem]">
-                  {project.description}
-                </p>
+
+                <div className="h-1/2 w-full flex flex-col gap-5 justify-center">
+                  <div className="flex justify-between items-center w-full">
+                    <h2 className="text-[#ffffff] text-2xl">{project.title}</h2>
+                    <div className="flex items-center justify-end gap-2 w-1/3">
+                      <a
+                        href={project.github}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <img
+                          src="/assets/icons/github-30.png"
+                          className="h-8 w-8
+                    inline bg-white rounded-full "
+                          alt="github"
+                        />{" "}
+                      </a>
+                      <a
+                        href={project.live}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <span
+                          className="bg-[#444444] hover:bg-[#232323] rounded-full text-xl "
+                          style={{
+                            padding: ".2rem .6rem",
+                            border: ".1rem solid white",
+                          }}
+                        >
+                          <i className="ri-arrow-right-up-long-line text-2xl " />
+                        </span>
+                      </a>
+                    </div>
+                  </div>
+                  <p className="text-[#bfbfbf] text-[1.1rem]">
+                    {project.description}
+                  </p>
+                </div>
               </div>
-            </div>
+            </AnimatedContent>
           </>
         );
       })}
